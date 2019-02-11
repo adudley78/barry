79ac8cde67609221cd05ef9373be744209967cf1
adudley78 <dudleyadam@gmail.com>
2019-02-10 21:34:54 -0500

    Built stacks#create action

--- app/controllers/stacks_controller.rb
+++ app/controllers/stacks_controller.rb
@@ -2,6 +2,7 @@ class StacksController < ApplicationController
 
   # render index.html.erb implicitly
   def index
+    @stack = Stack.new
     @stacks = Stack.all
     # is the controller able to get the stacks from the db
     # raise @stacks.inspect
@@ -11,8 +12,21 @@ class StacksController < ApplicationController
     @stack = Stack.find(params[:id])
   end
 
+  # Create a new Stack and associate it to the user
+  def create
+    @stack = Stack.new(stack_params)
+    @stack.name = params[:stack][:name]
+    # associate this Stack to the current user
+    # see recent lab I paired on
+    @stack.save
+
+    redirect_to stack_path(@stack)
+  end
+
   private
 
   # refactor @stack = Stack.find(params[:id]) to private method
-
+  def stack_params
+    params.require(:stack).permit(:name)
+  end
 end
