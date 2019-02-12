66d947e93924e05e57c333bd6d05bcda33fc462c
adudley78 <dudleyadam@gmail.com>
2019-02-12 10:38:51 -0500

    Add features tests

---
+++ spec/features/stack_addition.rb
@@ -0,0 +1,8 @@
+describe "stack addition", type: :feature do
+  before do
+    # visit new_stack_path
+    # fill_in "Story", with: "#storyname"
+    # select "tiny", from: "monster_size"
+    # fill_in "Taxonomy", with: "Abradacus nonexistus"
+    # click_button "Add Stack"
+  end
